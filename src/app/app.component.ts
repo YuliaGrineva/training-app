7f68b6ca451db24f0202d43d9049199955d9fcab
Julia <jgrinja@gmail.com>
2022-09-13 22:44:17 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'password-generator';
+  password: string = '';
+
+  onButtonClick() {
+    this.password = "my password"
+  }
+
+  openSidenav = false;
+
+
+ 
 }
